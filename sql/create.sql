e7333d6ee468d720e1b192252f3da9ac9ea75ab6
mmenaka24 <menaka_maruthainar@live.com>
2023-07-07 10:50:33 +0100

    added setup sql script

---
+++ sql/create.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Author (
+    Name varchar(128),
+    AuthorID SERIAL PRIMARY KEY
+);
+
+CREATE TABLE Member (
+    Username varchar(128),
+    AccessToken varchar(128),
+    UserID SERIAL PRIMARY KEY
+);
+
+
+CREATE TABLE Book (
+    ISBN varchar(13) PRIMARY KEY,
+    Title varchar(128),
+    AuthorID integer REFERENCES Author
+);
+
+CREATE TABLE Copy (
+    CopyID SERIAL PRIMARY KEY,
+    ISBN varchar(13) REFERENCES Book
+);
+
+CREATE TABLE Loan (
+    CopyID integer PRIMARY KEY references Copy,
+    UserID integer REFERENCES member,
+    DueDate date NOT NULL
+);
